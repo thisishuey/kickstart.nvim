aa95ee5933ce388bd798f90c1424e2adcda8c72d
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2024-05-01 03:27:34 +0000

    Revert back to original base16-vim

--- init.lua
+++ init.lua
@@ -778,14 +778,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rrethy/base16-nvim',
+    'thisishuey/base16-vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.opt.termguicolors = true
-      vim.cmd ':colorscheme base16-catppuccin-mocha'
+      vim.g.base16colorspace = 256
+      vim.cmd.colorscheme 'base16-default-dark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
