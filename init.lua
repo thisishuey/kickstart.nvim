63ac7b503a27a6628b425ecb39f781d68fa14a7d
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2024-05-05 00:38:49 +0000

    Add oil plugin

--- init.lua
+++ init.lua
@@ -190,6 +190,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Oil parent keybinding
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open [-]parent directory' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -664,6 +667,13 @@ require('lazy').setup({
         -- javascript = { { "prettierd", "prettier" } },
       },
     },
+  },
+
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 
   { -- Autocompletion
